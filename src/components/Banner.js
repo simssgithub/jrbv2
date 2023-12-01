aa9f01f673eed0de550bd23d23609d7f65b63afa
simssgithub <admin@simss.biz>
2023-12-01 15:50:52 +0800

    banner section working and done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,10 @@
 import React from "react";
-import myPic from "../assets/mypic.png"; // Adjust the import based on the correct path and filename
-import { FaGithub, FaLinkedin, FaTwitterSquare } from "react-icons/fa"; // Fix the import name here
+import Image from "../assets/mypic.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
@@ -12,10 +15,23 @@ const Banner = () => {
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           <div className="flex-1 text-center font-secondary lg:text-left">
-            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
               JAY <span>BAUTISTA</span>
-            </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary 
+          font-semibold uppercase leading-[1]"
+            >
               <span className="text-white mr-4">I am a</span>
               <TypeAnimation
                 sequence={[
@@ -31,36 +47,56 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-            </div>
-            <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
               {
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
               }
-            </p>
-            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
               <button className="btn btn-lg">Contact me</button>
-              <a
-                href="https://www.google.com"
-                className="text-gradient btn-link"
-              >
+              <a href="http://www.google.com" className="text-gradient btn-link">
                 My Portfolio
               </a>
-            </div>
-            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
-              <a href="https://github.com/simssgithub">
-                <FaGithub className="text-3xl" />
+            </motion.div>
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a href="https://github.com/simssgithub" target="new">
+                <FaGithub className="text-3xl"/>
               </a>
               <a href="https://www.linkedin.com/in/jay-bautista-bautista-aa8142b1/">
-                <FaLinkedin className="text-3xl" />
+                <FaLinkedin className="text-3xl"/>
               </a>
-              <a href="https://twitter.com/jrbwebdiebin">
-                <FaTwitterSquare className="text-3xl" />
+              <a href="https://twitter.com/jrbwebdiebin" target="new">
+                <FaSquareXTwitter className="text-3xl"/>
               </a>
-            </div>
+            </motion.div>
           </div>
-          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
-            <img src={myPic} alt="" />
-          </div>
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]"
+          >
+            <img src={Image} alt="" />
+          </motion.div>
         </div>
       </div>
     </section>
