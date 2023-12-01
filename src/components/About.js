cbd1d7dc25fabb034013bcd6df11823d692793a9
simssgithub <admin@simss.biz>
2023-12-01 18:32:31 +0800

    without isPending transition

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
-import React, { useTransition, useState } from "react";
+import React, { useState } from "react";
 import TabButton from "./TabButton";
-
 
 const TAB_DATA = [
   {
@@ -23,7 +22,9 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Doctor of Philosophy in Business Administration {"(Honoris Causa)"}</li>
+        <li>
+          Doctor of Philosophy in Business Administration {"(Honoris Causa)"}
+        </li>
         <li>Juris Doctor</li>
         <li>BS Information Technology</li>
       </ul>
@@ -47,55 +48,42 @@ const TAB_DATA = [
 
 const About = () => {
   const [tab, setTab] = useState("skills");
- const [isPending, startTransition] = useTransition();
 
   const handleTabChange = (id) => {
-    startTransition(() => {
-      setTab(id);
-    });
+    setTab(id);
   };
+
   return (
     <section className="text-white" id="about">
-    <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-    <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] 
-       bg-top w-full  object-cover rounded-lg'>
-      </div>
-      <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-        <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-        <p className="text-base lg:text-lg">
-          {
-            "I am a front end web developer with a passion for creating interactive and responsive web applications. I have experience working with .NET and C sharp for building web application, JavaScript and Bootstrap, HTML, CSS, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set as a matter of facts this portfolio was created on Nextjs and TailwindCSS also I'm building online shop using Shopify which currently i already publish and being use at the very moment. I am a team player and I am excited to work with others to create amazing applications. "
-          }
-        </p>
-        <div className="flex flex-row justify-start mt-8">
-          <TabButton
-            selectTab={() => handleTabChange("skills")}
-            active={tab === "skills"}
-          >
-            {" "}
-            Skills{" "}
-          </TabButton>
-          <TabButton
-            selectTab={() => handleTabChange("education")}
-            active={tab === "education"}
-          >
-            {" "}
-            Education{" "}
-          </TabButton>
-          <TabButton
-            selectTab={() => handleTabChange("certifications")}
-            active={tab === "certifications"}
-          >
-            {" "}
-            Certifications{" "}
-          </TabButton>
-        </div>
-        <div className="mt-8">
-          {TAB_DATA.find((t) => t.id === tab).content}
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <div
+          className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] 
+       bg-top w-full  object-cover rounded-lg"
+        ></div>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            {
+              "I am a front end web developer with a passion for creating interactive and responsive web applications. I have experience working with .NET and C sharp for building web application, JavaScript and Bootstrap, HTML, CSS, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set as a matter of facts this portfolio was created on Nextjs and TailwindCSS also I'm building online shop using Shopify which currently i already publish and being use at the very moment. I am a team player and I am excited to work with others to create amazing applications. "
+            }
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            {TAB_DATA.map((tabItem) => (
+              <TabButton
+                key={tabItem.id}
+                selectTab={() => handleTabChange(tabItem.id)}
+                active={tab === tabItem.id}
+              >
+                {tabItem.title}
+              </TabButton>
+            ))}
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 };
 
