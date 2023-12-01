957d27f21225d3b9a34a770adecc409dae64efd5
simssgithub <admin@simss.biz>
2023-12-01 10:16:31 +0800

    initss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './components/Header';
+// import components
+
+const App = () => {
+  return <div>
+    <Header/>
+  </div>;
+};
+
+export default App;
