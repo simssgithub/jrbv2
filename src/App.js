8f776831bbc888680232fd1c3deb5ee6e8ba4751
simssgithub <admin@simss.biz>
2023-12-01 09:35:49 +0800

    r

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }
